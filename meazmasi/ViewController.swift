69f7ec0cfd383b91a70659b340126ab88e0e421d
家 <Arisa@yamamotoarisa-no-macbook.local>
2016-11-17 20:18:20 +0900

    Initial Commit

---
+++ meazmasi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meazmasi
+//
+//  Created by 山本有紗 on 2016/11/17.
+//  Copyright © 2016年 山本有紗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
