ee779894b7e6d9db284aed0d30bf5a3d5d7e1bff
山本有紗 <Arisa@S-Arisa-YamamotonoMacBook-Pro.local>
2017-02-09 19:29:19 +0900

    ファイル追加

---
+++ meazmasi/Setting2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Setting2ViewController.swift
+//  meazmasi
+//
+//  Created by 山本有紗 on 2017/02/09.
+//  Copyright © 2017年 山本有紗. All rights reserved.
+//
+
+import UIKit
+
+class Setting2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
